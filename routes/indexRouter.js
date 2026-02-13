aca72b39e93d32e13e21046a105efe7a1f00bfb2
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2026-02-13 15:53:07 +0800

    setup: index get and log out route

---
+++ routes/indexRouter.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const indexController = require("../controllers/indexController");
+const indexRouter = Router();
+const isAuth = require("../lib/authMiddleware").isAuth;
+
+indexRouter.get("/", isAuth, indexController.indexGet);
+
+indexRouter.get("/log-out", indexController.logoutGet);
+
+module.exports = indexRouter;
