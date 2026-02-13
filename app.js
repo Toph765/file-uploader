53d13f7c745086b8cb4792a594b105df36df28ad
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2026-02-13 15:47:25 +0800

    setup: prisma session; router

--- app.js
+++ app.js
@@ -2,30 +2,44 @@ const express = require('express');
 const app = express();
 const path = require('node:path');
 const assetsPath = path.join(__dirname, 'public');
-const pool = require("./db/pool");
 const session = require("express-session");
 const passport = require("passport");
-const pgSession = require("connect-pg-simple")(session);
 require("dotenv").config();
 require("./lib/passport");
 
-const sessionStore = new pgSession({ pool });
+const prisma = require('./lib/prisma');
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+
+const loginRouter = require('./routes/loginRouter');
+const signupRouter = require('./routes/signupRouter');
+const indexRouter = require('./routes/indexRouter');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    store: sessionStore,
     cookie: {
         maxAge: 24 * 60 * 60 * 1000
     },
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(
+        prisma,
+        {
+            checkPeriod: 2 * 60 * 1000,
+            dbRecordIdIsSessionId: true,
+            dbRecordIdFunction: undefined
+        }
+    ),
 }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(assetsPath));
+
+app.use("/home", indexRouter);
+app.use("/", loginRouter);
+app.use("/sign-up", signupRouter);
 
 const PORT = process.env.PORT || 3000;
 
