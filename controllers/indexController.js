30753d1957943996b52f63f3cd3904f617cbba12
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2026-02-18 16:19:58 +0800

    feat: create folder function

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,22 @@
+const prisma = require('../lib/prisma');
+const { connect } = require('../routes/indexRouter');
+
 async function indexGet(req, res) {
-    res.render("index");
+    res.render("index", {userId: req.user.id});
+};
+
+async function createFolderPost(req, res) {
+    const { newFolder } = req.body;
+    const { userId } = req.params;
+
+    await prisma.folder.create({
+        data: {
+            name: newFolder,
+            userId: parseInt(userId)
+        },
+    });
+
+    res.redirect("/home");
 };
 
 async function logoutGet(req, res, next) {
@@ -14,5 +31,6 @@ async function logoutGet(req, res, next) {
 
 module.exports = {
     indexGet,
-    logoutGet
+    logoutGet,
+    createFolderPost
 }
