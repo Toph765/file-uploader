881a9674a6fc48cc53b83779e4c8f2c16d308700
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2026-02-21 16:23:17 +0800

    setup: upload post route

--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -2,8 +2,9 @@ const { Router } = require('express');
 const uploadController = require('../controllers/uploadController');
 const uploadRouter = Router();
 const isAuth = require('../lib/authMiddleware').isAuth;
+const upload = require('../controllers/uploadMiddleware');
 
-uploadRouter.get("/", isAuth, uploadController.uploadGet);
-// uploadRouter.post("/", uploadController.uploadPost);
+uploadRouter.get("/:userId", isAuth, uploadController.uploadGet);
+uploadRouter.post("/:userId", upload.single("filename"), uploadController.uploadPost);
 
 module.exports = uploadRouter;
