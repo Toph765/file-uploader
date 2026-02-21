9cf63bd16e9bca6fc3a91edbd9fb5f4050b9086f
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2026-02-21 16:24:37 +0800

    feat: upload file function

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,7 +1,47 @@
+const prisma = require('../lib/prisma');
+
 async function uploadGet(req, res) {
-    res.render("upload");
+    const folders = await prisma.folder.findMany({
+        where: {
+            userId: parseInt(req.params.userId),
+        }
+    })
+
+    console.log(folders)
+    res.render("upload", {userId: req.params.userId, folders});
 };
 
+async function uploadPost(req, res) {
+    const { folder } = req.body;
+    let folderObj = null;
+
+    if (folder !== "None") {
+    folderObj = await prisma.folder.findFirst({
+        where: {
+            name: folder,
+            userId: parseInt(req.params.userId),
+        },
+    });
+    }
+
+    const folderId = folderObj.id;
+
+    console.log('folderId: ', folderId);
+
+    await prisma.file.create({
+        data: {
+            fileName: req.file.originalname,
+            fileUrl: `uploads/${req.file.filename}`,
+            fileSize: req.file.size,
+            userId: parseInt(req.params.userId),
+            folderId
+        }
+    })
+
+    res.redirect("/home");
+}
+
 module.exports = {
-    uploadGet
+    uploadGet,
+    uploadPost
 }
