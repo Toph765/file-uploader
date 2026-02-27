f8f384b423156d8ac72ecfb1da58e638dc3dd9b1
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2026-02-27 17:59:49 +0800

    fix: remove params, typo, change var

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -3,37 +3,34 @@ const prisma = require('../lib/prisma');
 async function uploadGet(req, res) {
     const folders = await prisma.folder.findMany({
         where: {
-            userId: parseInt(req.params.userId),
+            userId: req.user.id,
         }
     })
 
-    console.log(folders)
-    res.render("upload", {userId: req.params.userId, folders});
+    res.render("upload", {userId: req.user.id, folders});
 };
 
 async function uploadPost(req, res) {
     const { folder } = req.body;
-    let folderObj = null;
+    let folderId = null;
 
-    if (folder !== "None") {
-    folderObj = await prisma.folder.findFirst({
+    if (folder !== "none") {
+    const folderObj = await prisma.folder.findFirst({
         where: {
             name: folder,
-            userId: parseInt(req.params.userId),
+            userId: req.user.id,
         },
     });
+
+    folderId = folderObj.id;
     }
-
-    const folderId = folderObj.id;
-
-    console.log('folderId: ', folderId);
 
     await prisma.file.create({
         data: {
             fileName: req.file.originalname,
             fileUrl: `uploads/${req.file.filename}`,
             fileSize: req.file.size,
-            userId: parseInt(req.params.userId),
+            userId: req.user.id,
             folderId
         }
     })
