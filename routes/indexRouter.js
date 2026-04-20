a702d6fdb0c0569171e8c1d3e52bce0b8210a5c0
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2026-04-20 17:26:45 +0800

    setup: route for folder

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -7,6 +7,7 @@ indexRouter.get("/", isAuth, indexController.indexGet);
 indexRouter.post("/new-folder", indexController.createFolderPost);
 indexRouter.get("/upload",indexController.uploaderGet);
 indexRouter.get("/:fileId/download", indexController.downloadGet);
+indexRouter.get("/:folderName", indexController.folderGet);
 
 indexRouter.get("/log-out", indexController.logoutGet);
 
